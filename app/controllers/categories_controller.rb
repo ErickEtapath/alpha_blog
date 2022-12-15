a063672a443d00f048fbe332a476a66774b3d9ca
Erick <erick@etapath.com>
2022-12-15 14:52:14 +0200

    Integration Tests for Create and Show New Category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,20 @@
 class CategoriesController < ApplicationController
+    before_action :set_category, only:[:show]
 
     def new
+        @category = Category.new
+    end
 
+    def create
+        @category = Category.new(category_params)
+
+        if @category.save
+            flash[:notice] = "Category Created"
+            redirect_to @category
+        else
+            flash[:alert] = "Category Failed to Create"
+            render :new, status: :unprocessable_entity
+        end
     end
 
     def index 
@@ -10,5 +23,14 @@ class CategoriesController < ApplicationController
 
     def show
 
+    end
+
+    private
+    def category_params
+        params.require(:category).permit(:name)
+    end
+
+    def set_category
+        @category = Category.find(params[:id])
     end
 end
